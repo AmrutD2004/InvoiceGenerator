d9375d54a4eb3dadc98ef94d397f07d90b88d2ba
Amrut Deshpande <amrutdeshpande2004@gmail.com>
2025-11-24 10:53:22 +0530

    Delete backend directory

--- backend/invoice/urls.py
+++
@@ -1,8 +0,0 @@
-
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path("user-register/", user_register, name='user-register'),
-    path("user-login/", user_login, name='user-login')
-]
